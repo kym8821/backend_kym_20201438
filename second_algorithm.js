631c02035c2bb694cc8643896f9c3ed51860f277
kym8821 <louie9798@naver.com>
2024-11-05 19:18:37 +0900

    [feat] solve second_algorithm.js

---
+++ second_algorithm.js
@@ -0,0 +1,23 @@
+function checkEmpty(value) {
+  let res = true;
+  if (Array.isArray(value)) {
+    value.forEach((v) => {
+      if (!checkEmpty(v)) res = false;
+    });
+  } else if (value === undefined || value === null || value === "") {
+    res = true;
+  } else if (typeof value === "object") {
+    Object.values(value).forEach((v) => {
+      if (!checkEmpty(v)) res = false;
+    });
+  } else {
+    return (res = false);
+  }
+  return res;
+}
+
+function isEmpty(value) {
+  ret = checkEmpty(value);
+  console.log(ret);
+  return ret;
+}
