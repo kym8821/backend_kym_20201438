693a0e8f046f42a5e99e5feb54ac51ae63257ee0
kym8821 <louie9798@naver.com>
2024-11-06 21:49:41 +0900

    [feat] handler error when check date format

--- back/src/util/checkDate.js
+++ back/src/util/checkDate.js
@@ -1,5 +1,10 @@
+// check valid date
 export function checkDate(date) {
   var format = /^(19[7-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
-  if (!format.test(date)) return false;
-  return true;
+  try {
+    if (!format.test(date)) return false;
+    return true;
+  } catch (err) {
+    return false;
+  }
 }
