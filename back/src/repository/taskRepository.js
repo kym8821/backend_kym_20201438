80c79efa8335810b2d74674e10c0f2bac13a4353
kym8821 <louie9798@naver.com>
2024-11-06 22:17:08 +0900

    [f]x update function to edit data based only on task ID to make it edit based on task ID and project ID.

--- back/src/repository/taskRepository.js
+++ back/src/repository/taskRepository.js
@@ -1,21 +1,27 @@
 // save task
 async function save(conn, saveDto) {
-  const query = 'INSERT INTO task(pjId, title, description, priority, dueDate, status) values(?, ?, ?, ?, ?, ?)';
+  const query = "INSERT INTO task(pjId, title, description, priority, dueDate, status) values(?, ?, ?, ?, ?, ?)";
   const param = Object.values(saveDto);
   const result = await conn.query(query, param);
   return result;
 }
 // find project by project id
 async function findByPjId(conn, pjId) {
-  const query = 'SELECT * FROM task WHERE pjId=?';
+  const query = "SELECT * FROM task WHERE pjId=?";
   const param = [pjId];
   const result = await conn.query(query, param);
   return result[0];
 }
 // find project by task id
 async function findById(conn, id) {
-  const query = 'SELECT * FROM task WHERE id=?';
+  const query = "SELECT * FROM task WHERE id=?";
   const param = [id];
+  const result = await conn.query(query, param);
+  return result[0];
+}
+async function findByTaskAndPj(conn, pjId, id) {
+  const query = "SELECT * FROM task WHERE id=? AND pjId=?";
+  const param = [id, pjId];
   const result = await conn.query(query, param);
   return result[0];
 }
@@ -24,7 +30,7 @@ async function update(conn, pjId, taskId, updateDto) {
   const param = Object.values(updateDto).concat([taskId, pjId]);
   const props = Object.keys(updateDto)
     .map((p) => `${p}=?`)
-    .join(',');
+    .join(",");
   const query = `UPDATE task SET ${props} WHERE id=? AND pjId=?`;
   const result = await conn.query(query, param);
   return result[0];
@@ -32,9 +38,9 @@ async function update(conn, pjId, taskId, updateDto) {
 // delete task by task id
 async function deleteById(conn, pjId, taskId) {
   const param = [taskId, pjId];
-  const query = 'DELETE FROM task WHERE id=? AND pjId=?';
+  const query = "DELETE FROM task WHERE id=? AND pjId=?";
   const result = await conn.query(query, param);
   return result[0];
 }
-const taskRepository = { save, findByPjId, findById, update, deleteById };
+const taskRepository = { save, findByPjId, findById, update, deleteById, findByTaskAndPj };
 export default taskRepository;
--- back/src/service/taskService.js
+++ back/src/service/taskService.js
@@ -1,9 +1,9 @@
-import connection from '../config/connection.js';
-import { ProjectResponseDto } from '../dto/project/projectResponseDto.js';
-import { ProjectUpdateDto } from '../dto/project/projectUpdateDto.js';
-import projectRepository from '../repository/projectRepository.js';
-import taskRepository from '../repository/taskRepository.js';
-import { transactionManager } from '../util/transactionManager.js';
+import connection from "../config/connection.js";
+import { ProjectResponseDto } from "../dto/project/projectResponseDto.js";
+import { ProjectUpdateDto } from "../dto/project/projectUpdateDto.js";
+import projectRepository from "../repository/projectRepository.js";
+import taskRepository from "../repository/taskRepository.js";
+import { transactionManager } from "../util/transactionManager.js";
 // transaction : save task
 async function save(saveDto) {
   const saveAndGet = async (conn, dto) => {
@@ -12,7 +12,11 @@ async function save(saveDto) {
     if (!insertInfo || !projects[0]) return undefined;
     const project = new ProjectResponseDto(projects[0]);
     const insertId = parseInt(insertInfo[0].insertId);
-    await projectRepository.updateProject(conn, project.id, new ProjectUpdateDto(undefined, undefined, JSON.stringify(project.tasks.concat([insertId]))));
+    await projectRepository.updateProject(
+      conn,
+      project.id,
+      new ProjectUpdateDto(undefined, undefined, JSON.stringify(project.tasks.concat([insertId])))
+    );
     const insertedTask = await taskRepository.findById(conn, insertId);
     return insertedTask[0];
   };
@@ -26,15 +30,16 @@ async function findAllByPjId(pjId) {
 }
 // transaction : update task by task id
 async function updateTask(pjId, taskId, updateDto) {
-  const updateAndGet = async (conn, dto) => {
-    await taskRepository.update(conn, pjId, taskId, dto);
-    const updatedTask = await taskRepository.findById(conn, taskId);
-    return updatedTask[0];
+  const updateAndGet = async (conn, pid, tid, dto) => {
+    await taskRepository.update(conn, pid, tid, dto);
+    const updatedTask = await taskRepository.findByTaskAndPj(conn, pid, taskId);
+    if (updatedTask.length === 0) return null;
+    else return updatedTask[0];
   };
-  const result = await transactionManager(connection, updateAndGet, [updateDto]);
+  const result = await transactionManager(connection, updateAndGet, [pjId, taskId, updateDto]);
   return result;
 }
-// not transaction : delete task by task id
+// transaction : delete task by task id
 async function deleteById(pjId, taskId) {
   const deleteAndUpdate = async (conn, pid, tid) => {
     const deleteResult = await taskRepository.deleteById(conn, pid, tid);
@@ -42,7 +47,11 @@ async function deleteById(pjId, taskId) {
     if (deleteResult.affectedRows > 0 && projects.length > 0) {
       const project = new ProjectResponseDto(projects[0]);
       project.tasks = project.tasks.filter((i) => i !== tid);
-      await projectRepository.updateProject(conn, pid, new ProjectUpdateDto(project.title, project.description, JSON.stringify(project.tasks)));
+      await projectRepository.updateProject(
+        conn,
+        pid,
+        new ProjectUpdateDto(project.title, project.description, JSON.stringify(project.tasks))
+      );
     }
     return deleteResult.affectedRows;
   };
